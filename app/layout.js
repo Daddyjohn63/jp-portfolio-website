2e60e3d27c741a6f6b0e76d537eac50f9e9535ab
Daddyjohn63 <john@webandprosper.co.uk>
2025-02-23 10:31:20 +0000

    added favicon

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,30 @@ export const metadata = {
     template: '%s | Web and Prosper'
   },
   description:
-    'Web and Prosper will help you get more clients and grow your business.'
+    'Web and Prosper will help you get more clients and grow your business.',
+  manifest: '/site.webmanifest',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' }
+    ],
+    apple: [
+      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
+    ],
+    other: [
+      {
+        url: '/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        url: '/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ]
+  }
 };
 
 // export const metadata = {
