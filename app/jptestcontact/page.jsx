9d05d488e8c9657a0feb7fee5469fb9b623e68a8
Daddyjohn63 <john@webandprosper.co.uk>
2025-03-30 15:33:32 +0100

    fixed build issues

--- app/jptestcontact/page.jsx
+++ app/jptestcontact/page.jsx
@@ -1,5 +1,5 @@
 'use client';
-import ReactCookieBot from 'react-cookiebot';
+
 import ContactForm from '@/components/contact/ContactForm';
 
 const cookiebotId = '008bf995-0224-4cfa-b2ff-4961832b2d91';
@@ -7,7 +7,6 @@ const cookiebotId = '008bf995-0224-4cfa-b2ff-4961832b2d91';
 const JpTestContact = () => {
   return (
     <div className="container mx-auto m-20">
-      {/* <ReactCookieBot id={cookiebotId} /> */}
       <ContactForm />
     </div>
   );
--- components/common/CookieConsent.js
+++ components/common/CookieConsent.js
@@ -1,75 +1,75 @@
-'use client';
-import CookieConsent from 'react-cookie-consent';
-import { useEffect } from 'react';
+// 'use client';
+// import CookieConsent from 'react-cookie-consent';
+// import { useEffect } from 'react';
 
-export default function CookieConsentBanner() {
-  useEffect(() => {
-    // Check if user has already made a choice
-    const consent = localStorage.getItem('website_cookie_consent');
-    if (consent === 'true') {
-      // If accepted, load Google Analytics
-      loadGoogleAnalytics();
-    }
-  }, []);
+// export default function CookieConsentBanner() {
+//   useEffect(() => {
+//     // Check if user has already made a choice
+//     const consent = localStorage.getItem('website_cookie_consent');
+//     if (consent === 'true') {
+//       // If accepted, load Google Analytics
+//       loadGoogleAnalytics();
+//     }
+//   }, []);
 
-  const loadGoogleAnalytics = () => {
-    // Create and load the GA script
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`;
-    script.async = true;
-    document.head.appendChild(script);
+//   const loadGoogleAnalytics = () => {
+//     // Create and load the GA script
+//     const script = document.createElement('script');
+//     script.src = `https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`;
+//     script.async = true;
+//     document.head.appendChild(script);
 
-    // Initialize GA
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      window.dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', process.env.NEXT_PUBLIC_GA_ID);
-  };
+//     // Initialize GA
+//     window.dataLayer = window.dataLayer || [];
+//     function gtag() {
+//       window.dataLayer.push(arguments);
+//     }
+//     gtag('js', new Date());
+//     gtag('config', process.env.NEXT_PUBLIC_GA_ID);
+//   };
 
-  const handleAccept = () => {
-    localStorage.setItem('website_cookie_consent', 'true');
-    loadGoogleAnalytics();
-  };
+//   const handleAccept = () => {
+//     localStorage.setItem('website_cookie_consent', 'true');
+//     loadGoogleAnalytics();
+//   };
 
-  const handleDecline = () => {
-    localStorage.setItem('website_cookie_consent', 'false');
-    // Disable Google Analytics
-    window['ga-disable-' + process.env.NEXT_PUBLIC_GA_ID] = true;
-  };
+//   const handleDecline = () => {
+//     localStorage.setItem('website_cookie_consent', 'false');
+//     // Disable Google Analytics
+//     window['ga-disable-' + process.env.NEXT_PUBLIC_GA_ID] = true;
+//   };
 
-  return (
-    <CookieConsent
-      location="bottom"
-      buttonText="Accept"
-      declineButtonText="Decline"
-      cookieName="website_cookie_consent"
-      style={{
-        background: '#2B373B',
-        fontSize: '14px',
-        padding: '1rem',
-        alignItems: 'center',
-        zIndex: 999
-      }}
-      buttonStyle={{
-        background: '#ffe4c4',
-        color: '#000',
-        fontSize: '14px',
-        padding: '0.5rem 1rem'
-      }}
-      declineButtonStyle={{
-        background: '#2B373B',
-        color: '#fff',
-        fontSize: '14px',
-        padding: '0.5rem 1rem'
-      }}
-      expires={365}
-      onAccept={handleAccept}
-      onDecline={handleDecline}
-    >
-      This website uses cookies to enhance your experience. By continuing to
-      visit this site you agree to our use of cookies.
-    </CookieConsent>
-  );
-}
+//   return (
+//     <CookieConsent
+//       location="bottom"
+//       buttonText="Accept"
+//       declineButtonText="Decline"
+//       cookieName="website_cookie_consent"
+//       style={{
+//         background: '#2B373B',
+//         fontSize: '14px',
+//         padding: '1rem',
+//         alignItems: 'center',
+//         zIndex: 999
+//       }}
+//       buttonStyle={{
+//         background: '#ffe4c4',
+//         color: '#000',
+//         fontSize: '14px',
+//         padding: '0.5rem 1rem'
+//       }}
+//       declineButtonStyle={{
+//         background: '#2B373B',
+//         color: '#fff',
+//         fontSize: '14px',
+//         padding: '0.5rem 1rem'
+//       }}
+//       expires={365}
+//       onAccept={handleAccept}
+//       onDecline={handleDecline}
+//     >
+//       This website uses cookies to enhance your experience. By continuing to
+//       visit this site you agree to our use of cookies.
+//     </CookieConsent>
+//   );
+// }
